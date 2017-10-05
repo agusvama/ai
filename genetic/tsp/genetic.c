3a128a779466846ddc23a6447be8f03e9b00c0b7
maldad <canond_7@hotmail.com>
2017-10-05 18:22:29 -0500

    merging 50 parents to produce 50 children

--- genetic/tsp/genetic.c
+++ genetic/tsp/genetic.c
@@ -1,19 +1,6 @@
 #include <stdlib.h>
 #include "lib/array.h"
-
-//do it with N nodes
-//parents
-int solution1[7] = {};
-int solution2[7] = {};
-int solution3[7] = {};
-int solution4[7] = {};
-int NODES = 7;
-
-//children
-int c1[7] = {};
-int c2[7] = {};
-int c3[7] = {};
-int c4[7] = {};
+#include "lib/variables.h"
 
 void setFromTo(int x[], int s){
   x[0] = s;
@@ -38,58 +25,52 @@ void createSolution(int x[], int startNode){
   }
 }
 
+void mergeNoPersisting(int a[], int y[], int persist){
+  int j = persist + 1;
+  for(int i = 1; i < NODES - 1; i++){
+    if(includes(a, y[i], NODES)){
+      continue;
+    }else{
+      a[j] = y[i];
+      j = j + 1;
+    }
+  }
+}
+
 void merge(int x[], int y[], int persist, int a[], int b[]){
   setFromTo(a, x[0]);
   setFromTo(b, y[0]);
-  int ja = persist + 1;
-  int jb = persist + 1;
 
   for(int i = 0; i <= persist; i++){
     a[i] = x[i];
     b[i] = y[i];
   }
-  for(int i = 1; i < NODES - 1; i++){
-    if(includes(a, y[i], NODES)){
-      continue;
-    }else{
-      a[ja] = y[i];
-      ja = ja + 1;
-    }
-  }
-  for(int i = 1; i < NODES - 1; i++){
-    if(includes(b, x[i], NODES)){
-      continue;
-    }else{
-      b[jb] = x[i];
-      jb = jb + 1;
-    }
-  }
+
+  mergeNoPersisting(a, y, persist);
+  mergeNoPersisting(b, x, persist);
 }
 
 int main(){
-  clearArrayWith(solution1, 0, NODES);
-  clearArrayWith(solution2, 0, NODES);
-  clearArrayWith(solution3, 0, NODES);
-  clearArrayWith(solution4, 0, NODES);
+  //creating solution
+  for(int i = 0; i < PARENTS; i++){
+    createSolution(parents[i], 1);
+  }
 
-  createSolution(solution1, 1);
-  createSolution(solution2, 1);
-  createSolution(solution3, 1);
-  createSolution(solution4, 1);
+  //merging
+  for(int i = 0; i < PARENTS; i = i + 2){
+    merge(parents[i], parents[i + 1], 2, children[i], children[i + 1]);
+  }
 
-  puts("parents");
-  printArray(solution1, NODES);
-  printArray(solution2, NODES);
-  puts("\nhijos");
-  merge(solution1, solution2, 2, c1, c2);
-  printArray(c1, NODES);
-  printArray(c2, NODES);
+  //printing results
+  for(int i = 0; i < PARENTS; i = i + 2){
+    puts("\nparents");
+    printArray(parents[i], NODES);
+    printArray(parents[i + 1], NODES);
 
-  puts("parents");
-  printArray(solution3, NODES);
-  printArray(solution4, NODES);
-  puts("\nhijos");
-  merge(solution3, solution4, 2, c3, c4);
-  printArray(c3, NODES);
-  printArray(c4, NODES);
+    puts("\nchildren");
+    /* printf("child: %d\n", i); */
+    printArray(children[i], NODES);
+    /* printf("child: %d\n", i + 1); */
+    printArray(children[i + 1], NODES);
+  }
 }
