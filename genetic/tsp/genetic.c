0a83efbf5057c2f00c114c707818f60ba3fd35d7
maldad <canond_7@hotmail.com>
2017-10-10 18:20:49 -0500

    trying with 10000 individuals and another matrix

--- genetic/tsp/genetic.c
+++ genetic/tsp/genetic.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <time.h>
 #include "lib/array.h"
 #include "lib/variables.h"
 #include "lib/number.h"
@@ -26,13 +27,13 @@ void createParents(int x[], int startNode){
   }
 }
 
-void mergeNoPersisting(int a[], int y[], int persist){
+void mergeNoPersisting(int parent[], int child[], int persist){
   int j = persist + 1;
   for(int i = 1; i < NODES - 1; i++){
-    if(includes(a, y[i], NODES)){
+    if(includes(child, parent[i], NODES)){
       continue;
     }else{
-      a[j] = y[i];
+      child[j] = parent[i];
       j = j + 1;
     }
   }
@@ -47,20 +48,22 @@ void merge(int x[], int y[], int persist, int a[], int b[]){
     b[i] = y[i];
   }
 
-  mergeNoPersisting(a, y, persist);
-  mergeNoPersisting(b, x, persist);
+  mergeNoPersisting(y, a, persist);
+  mergeNoPersisting(x, b, persist);
 }
 
 void mutate(int x[]){
-  int aux = x[2];
-  x[2] = x[3];
+  //sorry for this :/
+  int aux = x[1];
+  int aux2 = x[3];
   x[3] = aux;
+  x[1] = aux2;
 }
 
 void mutateChildren(){
   for(int i = 0; i < PARENTS; i++){
     int m = rand() % 10;
-    if(m <= 4){
+    if(m <= 8){
       mutate(children[i]);
     }else{
       continue;
@@ -80,14 +83,21 @@ int fitness(int x[]){
   return total;
 };
 
+void printGeneration(int generation, int index){
+  printf("result: %d fitness: %d\n", index, fitness(results[index]));
+  printArray(results[index], NODES);
+ }
+
 int main(){
   //creating parents
   for(int i = 0; i < PARENTS; i++){
     createParents(parents[i], 1);
   }
 
-  for(int i = 1; i <= GENERATIONS; i++){
-    printf("GENERATION: %d\n", i);
+  clock_t start, end;
+  start = clock();
+  for(int i = 0; i < GENERATIONS; i++){
+
     //merging
     for(int i = 0; i < PARENTS; i = i + 2){
       merge(parents[i], parents[i + 1], 2, children[i], children[i + 1]);
@@ -95,7 +105,7 @@ int main(){
 
     //applying mutation
     mutateChildren();
-    
+
     //comparing and creating a new population
     for(int i = 0; i < PARENTS; i++){
       //take the minor fitness and keep it in the new population 
@@ -106,24 +116,20 @@ int main(){
       }
     }
 
-    //testing, attention please, feel the tension...
+    //now the results become the new parents
     for(int i = 0; i < PARENTS; i++){
-      /* printf("parent [%d] fitness: %d\n", i, fitness(parents[i])); */
-      /* printArray(parents[i], NODES); */
-      /*  */
-      /* printf("child [%d] fitness: %d\n", i, fitness(children[i])); */
-      /* printArray(children[i], NODES); */
-
-      printf("result [%d] fitness: %d\n", i, fitness(results[i]));
-      printArray(results[i], NODES);
+      copyArray(results[i], parents[i], NODES);
     }
 
-      //now the results become the new parents
-      for(int i = 0; i < PARENTS; i++){
-        copyArray(results[i], parents[i], NODES);
-      }
+  }//GENERATIONS
 
-    puts("--------------------------------------------------------------------------");
+  //print last results...
+  puts("--------------------------HERE----------------------------------------------------");
+  for(int i = 0; i < PARENTS; i++){
+    printf("result[%d] fitness: %d\n", i, fitness(results[i]));
+    printArray(results[i], NODES);
   }
 
+  end = clock();
+  printf("Elapsed time: %f seconds\n", ((double) (end - start)) / CLOCKS_PER_SEC);
 }
