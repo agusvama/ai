4f769719e3a932a3370f3817356a0348d5b467b7
maldad <canond_7@hotmail.com>
2017-11-27 18:17:18 -0600

    i don't like this!

--- neuralNetwork/andTable/and.c
+++ neuralNetwork/andTable/and.c
@@ -9,8 +9,8 @@ int patterns [4][3]= {
   {1, 1, 1}
 };
 
-int ITERATIONS = 12;
 float THETA = 0.3;
+float ALPHA = 0.25;
 
 int zeroToThree(int x){
   if(x == 3){
@@ -31,19 +31,52 @@ int training(
   float bias,
   int iterations
 ){
-  if(iterations == ITERATIONS){
+  if(iterations == 0){
     return 1;
   }else{
     printf("x1  x2  w1   w2  θ    a    t  y  α(t - y)  dw1  dw2  dθ\n");
     printf("%d   %d   %.1f  %.1f %.1f %.1f  %d  %d   %.2f     %.2f %.2f %.2f \n",
-            patterns[p][0], patterns[p][1], w1, w2, THETA, a, patterns[p][2], y,
-            alphaByTminusY, dw1, dw2, bias, next(iterations));
-    printf("____________________________________________________________________\n");
-    return training(zeroToThree(p), w1, w2, a, y, alphaByTminusY, dw1, dw2, bias, next(iterations));
+            patterns[p][0], 
+            patterns[p][1], 
+            w1, 
+            w2, 
+            THETA, 
+            (w1 * patterns[p][0] + w2 * patterns[p][1]) - THETA, //a
+            patterns[p][2], //t
+            ( (w1 * patterns[p][0] + w2 * patterns[p][1]) - THETA ) > 0, //y
+            ALPHA*(patterns[p][2]-( (w1 * patterns[p][0] + w2 * patterns[p][1])-THETA > 0)),
+            ALPHA*(patterns[p][2]-( (w1 * patterns[p][0] + w2 * patterns[p][1])-THETA > 0)) * patterns[p][0],
+            ALPHA*(patterns[p][2]-( (w1 * patterns[p][0] + w2 * patterns[p][1])-THETA > 0)) * patterns[p][1],
+            ALPHA*(patterns[p][2]-( (w1 * patterns[p][0] + w2 * patterns[p][1])-THETA > 0))
+            );
+    /* printf("--------------------------------------------------------------------\n"); */
+    return training(
+      zeroToThree(p),
+      w1 + ALPHA*(patterns[p][2]-( (w1 * patterns[p][0] + w2 * patterns[p][1])-THETA > 0)),
+      w2 + ALPHA*(patterns[p][2]-( (w1 * patterns[p][0] + w2 * patterns[p][1])-THETA > 0)),
+      (w1 * patterns[p][0] + w2 * patterns[p][1]) - THETA, //a
+      ( (w1 * patterns[p][0] + w2 * patterns[p][1]) - THETA ) > 0, //y
+      ALPHA * (patterns[p][2] - ( (w1 * patterns[p][0] + w2 * patterns[p][1])-THETA > 0) ),
+      ALPHA*(patterns[p][2]-( (w1 * patterns[p][0] + w2 * patterns[p][1])-THETA > 0)) * patterns[p][0],
+      ALPHA*(patterns[p][2]-( (w1 * patterns[p][0] + w2 * patterns[p][1])-THETA > 0)) * patterns[p][1],
+      ALPHA*(patterns[p][2]-( (w1 * patterns[p][0] + w2 * patterns[p][1])-THETA > 0)),
+      previous(iterations)
+      );
   }
 }
 
 int main(){
-//training(p  w1   w2    a    y    alphaByTminusY dw1  dw2  bias iterations
-  training(0, 0.0, 0.0, -0.3, 0.0, 0.0,           0.0, 0.0, 0.0, 0);
+  //training(p  w1   w2    a    y    alphaByTminusY dw1  dw2  bias iterations
+  training(
+      0,
+      0.0, //w1
+      0.0, //w2
+      (0.0 * patterns[0][0] + 0.0 * patterns[0][1]) - THETA, //a
+      ( (0.0 * patterns[0][0] + 0.0 * patterns[0][1]) - THETA ) > 0, //y
+      ALPHA * ( patterns[0][2] - ( (0.0 * patterns[0][0] + 0.0 * patterns[0][1]) - THETA > 0)), //alpha by t minus y
+      ALPHA * ( patterns[0][2] - ( (0.0 * patterns[0][0] + 0.0 * patterns[0][1]) - THETA > 0)) * patterns[0][0], // dw1
+      ALPHA * ( patterns[0][2] - ( (0.0 * patterns[0][0] + 0.0 * patterns[0][1]) - THETA > 0)) * patterns[0][1], //dw2
+      ALPHA * ( patterns[0][2] - ( (0.0 * patterns[0][0] + 0.0 * patterns[0][1]) - THETA > 0)), //bias
+      12 //iterations
+      );
 }
